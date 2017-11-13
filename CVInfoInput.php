9a0b1326c626a8fce824cc5aa412ad246dbcf8f9
Daquaris Chadwick <sourhead5@gmail.com>
2017-11-12 23:58:44 -0500

    added pdf creation functionality and changed some html code

--- CVInfoInput.php
+++ CVInfoInput.php
@@ -8,16 +8,16 @@
 	<h1><a href = "Index.PHP">CV Creation Software</a></h1>
 	<h2>CV Information Input</h2>
 	<h3>Personal Information</h3>
-	<form>
+	<form action="CreateCV.php" method="post">
 		Name:<br>
-		<input type = "text" name = "First and Last"><br>
+		<input type = "text" name="name"><br>
 		Phone Number:<br>
-		<input type = "tel" name = "Phone Number"><br>
+		<input type = "tel" name="phone"><br>
 		Email:<br>
-		<input type = "email" name = "Email"><br>
-	</form>
-	<h3>Employment History</h3>
-	<form id = "EmploymentHistoryForm">
+		<input type="email" name="email"><br>
+
+
+		<h3>Employment History</h3>
 		Work History:<br>
 		<textarea form = "EmploymentHistoryForm" name = "WorkHistory" rows = "10" cols = "50" placeholder = "Enter work history here..">	
 		</textarea><br>
@@ -27,57 +27,63 @@
 		Research and Training:<br>
 		<textarea form = "EmploymentHistoryForm" name = "Reasearch" rows = "10" cols = "50" placeholder = "Enter any research or training here..">	
 		</textarea><br>
+
+
+		<h3>Education</h3>
+			University Attended:<br>
+			<input type = "text" name = "University"><br>
+			Degree:<br>
+			<input type = "text" name = "Degree"><br>
+			Major:<br>
+			<input type = "text" name = "Major"><br>
+
+
+		<h3>Professional Qualifications</h3>
+			Certifications:<br>
+			<textarea form = "QualificationsForm" name = "Certs" rows = "10" cols = "50" placeholder = "Enter certifications here..">	
+			</textarea><br>
+			Accreditations:<br>
+			<textarea form = "QualificationsForm" name = "Accreds" rows = "10" cols = "50" placeholder = "Enter accreditations here..">	
+			</textarea><br>
+
+
+		<h3>Choose Template</h3>
+		<table>
+			<tr>
+				<td>
+            		<img src="images/placeholder.jpg" alt="template1">
+            		<figcaption>Template 1</figcaption>
+        		</td>
+				<td>
+            		<img src="images/placeholder.jpg" alt="template2">
+            		<figcaption>Template 2</figcaption>
+				</td>
+				<td>
+            		<img src="images/placeholder.jpg" alt="template3">
+            		<figcaption>Template 3</figcaption>
+				</td>            
+			</tr>
+			<tr>
+				<td>
+            		<img src="images/placeholder.jpg" alt="template4">
+            		<figcaption>Template 4</figcaption>
+				</td>
+				<td>
+            		<img src="images/placeholder.jpg" alt="template5">
+            		<figcaption>Template 5</figcaption>
+				</td>
+			</tr>
+		</table>
+
+
+		<input type = "radio" name="template" value="template1">Template 1<br>
+		<input type = "radio" name="template" value="template2">Template 2<br>
+		<input type = "radio" name="template" value="template3">Template 3<br>
+		<input type = "radio" name="template" value="template4">Template 4<br>
+		<input type = "radio" name="template" value="template5">Template 5<br><br>
+		<input type = "checkbox" name="pdfChoose" value="Download as PDF?">Download CV as PDF?<br><br>
+
+
+		<input type="submit" name="submit" value="cv">
 	</form>
-	<h3>Education</h3>
-	<form>
-		University Attended:<br>
-		<input type = "text" name = "University"><br>
-		Degree:<br>
-		<input type = "text" name = "Degree"><br>
-		Major:<br>
-		<input type = "text" name = "Major"><br>
-	</form>
-	<h3>Professional Qualifications</h3>
-	<form id = "QualificationsForm">
-		Certifications:<br>
-		<textarea form = "QualificationsForm" name = "Certs" rows = "10" cols = "50" placeholder = "Enter certifications here..">	
-		</textarea><br>
-		Accreditations:<br>
-		<textarea form = "QualificationsForm" name = "Accreds" rows = "10" cols = "50" placeholder = "Enter accreditations here..">	
-		</textarea><br>
-	</form>
-	<h3>Choose Template</h3>
-	<table>
-        <tr>
-            <td>
-            	<img src="images/placeholder.jpg" alt="template1">
-            	<figcaption>Template 1</figcaption>
-        	</td>
-            <td>
-            	<img src="images/placeholder.jpg" alt="template2">
-            	<figcaption>Template 2</figcaption>
-            </td>
-            <td>
-            	<img src="images/placeholder.jpg" alt="template3">
-            	<figcaption>Template 3</figcaption>
-            </td>            
-        </tr>
-        <tr>
-            <td>
-            	<img src="images/placeholder.jpg" alt="template4">
-            	<figcaption>Template 4</figcaption>
-            </td>
-            <td>
-            	<img src="images/placeholder.jpg" alt="template5">
-            	<figcaption>Template 5</figcaption>
-            </td>
-        </tr>
-    </table>
-    <input type = "radio" name="template" value="template1">Template 1<br>
-    <input type = "radio" name="template" value="template2">Template 2<br>
-    <input type = "radio" name="template" value="template3">Template 3<br>
-    <input type = "radio" name="template" value="template4">Template 4<br>
-    <input type = "radio" name="template" value="template5">Template 5<br><br>
-    <input type = "checkbox" name="pdfChoose" value="Download as PDF?">Download CV as PDF?<br><br>
-	<input type = "submit" value = "Create CV">
 </html>
--- CreateCV.php
+++ CreateCV.php
@@ -42,9 +42,66 @@
 	{
 		$pdf = new FPDF();
 		$pdf->AddPage();
-		$pdf->SetFont('Times', '', $theme->header->fontsize);
-		$pdf->Cell(40, 10, 'Hello World');
-		$pdf->Cell(60,10,'Powered by FPDF.',0,1,'C');
+		$pdf->SetFont($theme['header']['font'], '', $theme['header']['fontsize']);
+
+		// Create Header if CV
+		$pdf->Cell(80);
+		$pdf->Cell(10,5,$report['name'],0,1,'C');
+		$pdf->Cell(80);
+		$pdf->Cell(10,5,$report['phone'],0,1,'C');
+		$pdf->Cell(80);
+		$pdf->Cell(10,5,$report['email'],0,1,'C');
+
+
 		$pdf->Output();
+	}
+
+	/**
+	* GetTheme
+	* Retrieve theme '$themeName' from database
+	*
+	*
+	* $themeName string name of the theme
+	* return array theme array 
+	*/
+	function GetTheme($themeName)
+	{
+		// Query DB for theme information
+
+		// Set theme information into theme array in respective positions
+		$theme = [
+			'header' => [
+				'x' => 0,
+				'y' => 0,
+				'color' => '#FF0000',
+				'backgroundcolor' => '#FF0000',
+				'fontsize' => 10,
+				'font' => 'Arial'
+			],
+			'body' => [
+			
+			],
+			'footer' => [
+			
+			]
+		];
+
+		// Return the theme array
+		return $theme;
+	}
+
+	if(isset($_POST['submit']))
+	{
+
+		// Get the theme to use for the CV
+		$theme = GetTheme('theme-01');
+
+		$report = [
+			'name' => htmlspecialchars($_POST['name']),
+			'phone' => htmlspecialchars($_POST['phone']),
+			'email' => htmlspecialchars($_POST['email'])
+		];
+
+		CreateCV($theme, $report);
 	}
 ?>
