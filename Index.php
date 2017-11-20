5e47540f007e77d62c3c67c7240646958f1bc731
Josh Pollmann <joshpollmann102@gmail.com>
2017-11-20 14:51:39 -0500

    Implement admin controls

--- Index.php
+++ Index.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="en-US">
+<?php
+    session_set_cookie_params(0);
+    session_start();
+?>
 <html>
 <head>
 	<title>CV Creation Software</title>
@@ -7,12 +11,15 @@
 </head>
 <body>
 	<h1>CV Creation Software</h1>
-    <a href="Login.PHP">
-        <h3>Login</h3>
-    </a>
-    <a href="CreateAccount.PHP">
-        <h3>Sign Up</h3>
-    </a>
+    <?php if(isset($_SESSION['admin']) && $_SESSION['admin']): ?>
+        <a href="AdminControls.PHP"><h4>Admin Tools</h4></a>
+    <?php endif; ?>
+    <?php if(isset($_SESSION['logged_in'])): ?>
+        <?php echo "<p>Logged in as " . $_SESSION['login_user'] . "</p>"; ?>
+    <?php else: ?>
+        <a href="Login.PHP"><h3>Login</h3></a>
+        <a href="CreateAccount.PHP"><h3>Sign Up</h3></a>
+    <?php endif; ?>
     <ul>
         <a href="TemplatePreview.PHP">
             <li>View Sample Templates</li>
@@ -20,6 +27,13 @@
         <a href="CVInfoInput.PHP">
             <li>Create a CV</li>
         </a>
+        <?php if(isset($_SESSION['logged_in'])): ?>
+            <a href="ViewSaved.PHP"><li>View Saved Templates</li></a>
+            <a href="EditSaved.PHP"><li>Edit Saved CV</li></a>
+        <?php endif; ?>
     </ul>
+    <?php if(isset($_SESSION['logged_in'])): ?>
+        <a href="Logout.PHP"><h4>Logout</h4></a>
+    <?php endif; ?>
 </body>
 </html>
--- Login.php
+++ Login.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="en-US">
+<?php 
+	session_set_cookie_params(0);
+	session_start(); 
+?>
 <html>
 <head>
 	<title>CV Creation Software</title>
@@ -8,13 +12,49 @@
 <body>
 	<h1><a href = "Index.PHP">CV Creation Software</a></h1>
 	<h1>Login</h1>
-	<form>
+	<?php
+		require "Config.PHP";
+		$error = '';
+		if(isset($_POST['submit']) && !empty($_POST['username']) && !empty($_POST['pass']))
+		{
+			$username = htmlspecialchars($_POST['username']);
+			$password = htmlspecialchars($_POST['pass']);
+
+			$sql = "SELECT Admin FROM users WHERE Username = '$username'  AND Password = '$password'";
+			$result = $conn->query($sql);
+
+			if($result->num_rows > 0)
+			{
+				// success
+				while($row = $result->fetch_assoc())
+				{
+					$admin = $row["Admin"];
+				}
+				if($admin) $_SESSION['admin'] = TRUE;
+				else $_SESSION['admin'] = FALSE;
+				$_SESSION['logged_in'] = TRUE;
+				$_SESSION['login_user'] = $username;
+				header("Location: SuccessfulLogin.PHP");
+				$conn->close();
+				exit();
+			}else
+			{
+				// no such account
+				$error = 'Incorrect username or password1';
+			}
+		}else
+		{
+			$error = 'Please enter a username and password';
+		}
+	?>
+	<form class="login_form" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
 		Username:<br>
-		<input type = "text" name = "username"><br>
+		<input type = "text" name = "username" required><br>
 		Password:<br>
-		<input type = "password" name = "pass">
-		<input type = "submit" value = "Submit">
+		<input type = "password" name = "pass" required>
+		<input type = "submit" name = "submit" value = "Submit">
 		<input type = "reset" value = "Clear">
+		<h4 class = "error"><?php echo $error; ?></h4>
 	</form>
 	<p>Don't have an account? <a href = "CreateAccount.PHP">Create one</a></p>
 </body>
