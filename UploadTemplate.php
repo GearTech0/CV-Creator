95d86c4a7dbe690c729f00579ff5e63f922de8e5
Daquaris Chadwick <sourhead5@gmail.com>
2017-11-30 17:28:00 -0500

    new branch created to keep changes on working docxreader

--- UploadTemplate.php
+++ UploadTemplate.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en-US">
 <?php
     session_set_cookie_params(0);
@@ -46,10 +46,8 @@
 				{
                     require "docxtotext.php";
                     require "Config.PHP";
-
                     $docObj = new Doc2Txt($filename);
                     $docText = $docObj->convertToText();
-
                     // Tracking Variables
                     $inside = false;
                     $inBox = false;
@@ -60,10 +58,8 @@
                     $value = "";
                     $colValue = "";
                     $template = [];
-
                     for($i=0;$i<strlen($docText);$i++)
                     {
-                        echo $docText{$i};
                         switch($docText{$i})
                         {
                             case '@':
@@ -74,7 +70,6 @@
                                     $setValue = false;
                                     $colValue = $value;
                                     $value = "";
-
                                     $template[$colName] = $colValue;
                                 }
                                 break;
@@ -83,12 +78,14 @@
                                 if($setName)
                                 {
                                     $setName = false;
-                                    $colName = utf8_encode($value);
+                                    $colName = $value;
                                     $setValue = true;
                                     $value = "";
                                 }else{
                                     $inside = $setName = $setValue = $d = false;
                                 }
+                                break;
+                            case ' ':
                                 break;
                             default:
                                 if($inside)
@@ -96,25 +93,26 @@
                                 break;
                         }
                     }
-
                     $keys = "";
                     $values = "";
-                    for($template as $key => $value)
+                    foreach($template as $key => $value)
                     {
-                        $keys .= $key . ",";
-                        if(intval($value))
-                            $values .= $value . ",";
+                        if($value == "") continue;
+                        $keys .= $conn->real_escape_string($key) . ", ";
+                        if(intval($value) || $value == '0')
+                            $values .=  $conn->real_escape_string($value) . ",";
                         else
-                            $values .= '"' . $value . '",';
+                            $values .= "'" . $conn->real_escape_string($value) . "',";
                     }
-                    $keys{strlen($keys)-1} = '';
-                    $values{strlen($values)-1} = '';
-
-                    $sql = "INSERT INTO " . $_SESSION['login_user'] . "_templates (" . $keys . ") VALUES (" . $values . ");";
+                    $keys = substr($keys, 0, strlen($keys)-2);
+                    $values = substr($values, 0, strlen($values)-1);
+                    $sql = "INSERT INTO `" . $_SESSION['login_user'] . "_templates` ({$keys}) VALUES ({$values});";
                     if($conn->query($sql))
-                        $error = $filename . ' uploaded successfully. ';
+                    {
+                        $error = $filename . ' uploaded successfully.';
+                    }
                     else
-                        $error = "Error sending template to database.";
+                        $error = "Error sending template to database." . $conn->error;
 				}else
 				{
 					$error = 'Upload failed';
